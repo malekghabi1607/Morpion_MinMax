aced2057cbbe10fee9191df9be0c23c2417aaa5d
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-04-01 16:04:17 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/****************************************************************/
+/* Auteur : GHABI Malek						                    */
+/* Date : 14/11/2024				                */
+/****************************************************************/
+
+
+
+
+
+#include "tictactoe.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main () {
+
+    TicTacToe partie;
+    // Génération d'un nombre aléatoire entre 0 et 1
+    srand(time(NULL));
+    int piece = rand() % 2;
+    int bestMove = 0;
+    int move = 0;
+    partie.Affichage();
+    while (partie.arbitre() == -2) {
+    if (piece == 0) {
+        cout << "C'est à l'ordinateur de jouer ! (symbole : X)" << endl;
+        if (partie.arbitre() == -2) {
+            partie.jeuOrdi(bestMove);
+            partie.play(bestMove, false);
+            partie.Affichage();
+            piece = 1; // Passe au tour du joueur
+        }
+    } else {
+        if (partie.arbitre() == -2) {
+            cout << "C'est à vous de jouer ! (symbole : O)" << endl;
+            cout << "Entrez le numero de la case que vous voulez jouer : ";
+            cin >> move;
+            while (!partie.play(move, true)){
+                cout << "Entrez un numero de case valide : ";
+                cin >> move;
+            }
+            partie.Affichage();
+            piece = 0; // Passe au tour de l'ordinateur
+        }
+    }
+}
+
+        if (partie.arbitre() == 1){
+            cout << "L'ordinateur a gagné !" << endl;
+            partie.Affichage();
+        } else if (partie.arbitre() == -1) {
+            cout << "Vous avez gagné !" << endl;
+            partie.Affichage();
+        } else if (partie.arbitre() == 0) {
+            cout << "Match nul !" << endl;
+            partie.Affichage();
+        }
+    return 0;
+}
---
+++ tictactoe.cpp
@@ -0,0 +1,162 @@
+/****************************************************************/
+/* Auteur : GHABI Malek						                    */
+/* Date : 14/11/2024				                */
+/****************************************************************/
+
+
+#include "tictactoe.h"
+#include <iostream>
+using namespace std;
+
+// Constructor
+TicTacToe::TicTacToe(){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            T[i][j]=0;
+        }
+    }
+}
+
+// afficher le jeu
+void TicTacToe::Affichage(){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if (T[i][j]==0){
+                cout<<"- ";
+            }else if (T[i][j]==1){
+                cout<<"X ";
+            }else{
+                cout<<"O ";
+            }
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+// jeu de l'ordinateur
+int TicTacToe::jeuOrdi(int & bestMove){
+    int arg;
+    if (this->plein()){ // si la partie est finie
+        return 0; 
+    }
+    if (this->arbitre()== 1){ // si l'ordinateur a gagné
+        return 1;
+    }
+    int val = -1;
+    for (int i=0;i<9;i++){
+        if (T[i/3][i%3]==0){
+            play(i, false);
+            int res = this->jeuHumain(arg);
+            unplay(i);
+            if (res > val){
+                val = res;
+                bestMove = i;
+            }
+        }
+    }
+    return val;
+}
+
+// jeu de l'humain
+int TicTacToe::jeuHumain(int & bestMove){
+    int arg = 0;
+    if (this->plein()){ // si la partie est finie
+        return 0; 
+    }
+    if (this->arbitre()== -1){ // si l'humain a gagné
+        return -1;
+    }
+    int val = 1;
+    int res;
+    for (int i=0;i<9;i++){
+        if (T[i/3][i%3]==0){
+            play(i, true);
+            res = this->jeuOrdi(arg);
+            unplay(i);
+            if (res < val){
+                val = res;
+                bestMove = i;
+            }
+        }
+    }
+    return val;
+}
+
+// arbitrer le jeu
+int TicTacToe::arbitre(){
+    int score = 0;
+    for(int i=0;i<3;i++){ // vérifier les lignes
+        if(T[i][0]==T[i][1] && T[i][1]==T[i][2]){
+            if(T[i][0]==1){
+                return 1;
+            }else if(T[i][0]==-1){
+                return -1;
+            }
+        }
+    }
+    for(int j=0;j<3;j++){ // vérifier les colonnes
+        if(T[0][j]==T[1][j] && T[1][j]==T[2][j]){
+            if(T[0][j]==1){
+                return 1;
+            }else if(T[0][j]==-1){
+                return -1;
+            }
+        }
+    }
+    if(T[0][0]==T[1][1] && T[1][1]==T[2][2]){ // vérifier les diagonales
+        if(T[0][0]==1){
+            return 1;
+        }else if(T[0][0]==-1){
+            return -1;
+        }
+    }
+    if(T[0][2]==T[1][1] && T[1][1]==T[2][0]){ // vérifier les diagonales
+        if(T[0][2]==1){
+            return 1;
+        }else if(T[0][2]==-1){
+            return -1;
+        }
+    }
+    for(int i=0;i<3;i++){ // vérifier si la partie est finie
+        for(int j=0;j<3;j++){
+            if(T[i][j]==0){
+                score = -2;
+            }
+        }
+    }
+    return score;
+}
+
+// jouer
+bool TicTacToe::play(int bestMove, bool joueur){
+    int i = bestMove/3;
+    int j = bestMove%3;
+    if (T[i][j] == 0 && i<3 && j<3){
+        if(joueur){
+            T[i][j]=-1;
+        }else{
+            T[i][j]=1;
+        }
+        return true;
+    }
+    return false;
+}
+
+// annuler le coup
+void TicTacToe::unplay(int bestMove){
+    int i = bestMove/3;
+    int j = bestMove%3;
+    T[i][j]=0;
+}
+
+bool TicTacToe::plein(){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(T[i][j]==0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
